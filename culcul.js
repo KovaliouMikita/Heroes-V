b334ce572e3db141386a55da86bb72383ceef6e2
KovaliouMikita <124069485+KovaliouMikita@users.noreply.github.com>
2024-02-22 18:17:47 +0300

    Add files via upload

--- culcul.js
+++ culcul.js
@@ -1,28 +1,39 @@
 import {unitData} from "./modules/object.js";
 
+let div = document.querySelectorAll("div")
+let name = document.querySelector('.name')
+let att = document.querySelector('.att')
+let def = document.querySelector('.def')
+let dmg = document.querySelector('.dmg')
+let hp = document.querySelector('.hp')
+let spd = document.querySelector('.spd')
+let int = document.querySelector('.int')
+let shot = document.querySelector('.shot')
+let mana = document.querySelector('.mana')
+let spell = document.querySelector('.spell')
 
-//.textContent = ( ((unitData[img.id])[type])['hp'] )
 
-document.addEventListener('click', (event) => {
-    let div = document.querySelectorAll("div")
-    
+document.addEventListener('click', (event) => {   
     const type = event.target.dataset.type
+    console.log (event.target)
         for (let img of div){
         if (img.id ){
             if (!(unitData[img.id])[type]) continue
-        console.log( ((unitData[img.id])[type])['att'] )
-    document.querySelector('.name').textContent = ((unitData[img.id])[type])['name']
-      document.querySelector('.centralArea').children[1].children[0].children[0].children[1].textContent = ((unitData[img.id])[type])['att'] 
-      document.querySelector('.centralArea').children[1].children[0].children[0].children[2].textContent = ''
-      document.querySelector('.centralArea').children[1].children[0].children[1].children[1].textContent = ((unitData[img.id])[type])['def']
-      document.querySelector('.centralArea').children[1].children[0].children[1].children[2].textContent = ''
-      document.querySelector('.centralArea').children[1].children[0].children[2].children[1].textContent = ((unitData[img.id])[type])['dmg'][0]
-      document.querySelector('.centralArea').children[1].children[0].children[2].children[2].textContent = '-'+((unitData[img.id])[type])['dmg'][1] 
-      
-      
+    
+            name.textContent = ((unitData[img.id])[type])['name']
+            att.children[1].textContent = ((unitData[img.id])[type])['att'] 
+            def.children[1].textContent = ((unitData[img.id])[type])['def']
+
+            dmg.children[1].textContent = ((unitData[img.id])[type])['dmg'][0] + '-' + ((unitData[img.id])[type])['dmg'][1] 
+            hp.children[1].textContent =  ((unitData[img.id])[type])['hp']
+            spd.children[1].textContent =  ((unitData[img.id])[type])['spd']
+            int.children[1].textContent =  ((unitData[img.id])[type])['int']
+            shot.children[1].textContent =  ((unitData[img.id])[type])['shot']
+            mana.children[1].textContent =  ((unitData[img.id])[type])['mana']
+            spell.children[1].textContent =  ((unitData[img.id])[type])['spell']
+
+            break
         }
 
     } 
-    let div1 = document.querySelectorAll("td[img^='m1_1']")
-    div1.background = 'black' 
   })
